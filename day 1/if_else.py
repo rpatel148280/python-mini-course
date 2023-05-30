55af1e20961e4c9da8e6849aafdf55ba6e862064
Rishu Patel <rpatel148280@me.bergen.edu>
2023-05-30 17:15:14 -0400

    completed day 1

--- day 1/if_else.py
+++ day 1/if_else.py
@@ -4,7 +4,21 @@
 
 # if, elif, else
 
-# and
+if True:
+    print("It's true")
+
+if False:
+    print("It's false")
+
+
+    num = 1
+    print("Num is ", num)
+    if num == 0:
+        print("It's 0!")
+    if num == 1:
+        print("It's 1!")
+
+# &(and)
 
 # or
 
