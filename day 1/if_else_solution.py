00365e87af073da3a44baaa7df7de72cb4f6a93a
Rishu Patel <rpatel148280@me.bergen.edu>
2023-06-01 17:08:42 -0400

    completed day 2

--- day 1/if_else_solution.py
+++ day 1/if_else_solution.py
@@ -11,17 +11,26 @@
 #     print("It's false")
 
 
-num = 1
+num = 4
 print("Num is ", num)
 if num == 0:
     print("It's 0!")
-if num == 1:
+elif num == 1:
     print("It's 1!")
+else:
+    print("I don't know what num is but it's not 1 or 0")
 
 # and
 
-# or
+# if True and False:
 
+
+
+# or
+# if True or False
+#    print("True or Flase is True")
+# else:
+#    print("True or False is false")
 # not
 
 # comparision == !=
--- day 2/rockScissorsPaper.py
+++ day 2/rockScissorsPaper.py
@@ -14,10 +14,45 @@ def computerChoice():
   return random.randint(0, 2)
 
 def rockScissorsPaper():
-  #Code Here
+  #code here
+  #ask user for a choice of rock scissor paper
+  user_choice = input("Choose rock, paper or scissor: ")
+  print(user_choice)
+
+  # if statement that checks if uer's choice is correct
+  if user_choice == "Rock" or user_choice == "Scissors" or user_choice == "Paper":
+    print("Your response is valid")
+  else:
+    print("Your response is not valid")
+
+  #computer choice
+  computer_choice = whatIsIt(computerChoice())
+  print("computer choice: ")
+  print(computer_choice)
+
+  # print tie if user and computer response is the same
+  if user_choice == computer_choice:
+    print("It's a tie!")
+  else:
+    if user_choice == "Rock":
+      if computer_choice == "Scissors":
+        print("The user won!")
+      if computer_choice == "Paper":
+        print("The computer won!")
+    elif user_choice == "Scissors":
+      if computer_choice == "Rock":
+        print("The computer won!")
+      if user_choice == "Paper":
+        print("The user won!")
+    elif user_choice == "Paper":
+      if user_choice == "Rock":
+        print("The user won!")
+      if user_choice == "Scissors":
+        print("The computer won!")
+
 
   #print random computer response
-  print(whatIsIt(computerChoice()))
+  #print(whatIsIt(computerChoice()))
 
 rockScissorsPaper()
 
